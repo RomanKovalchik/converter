d78425449c90be651e38a71f2b9f430d18127b5d
RomanKovalchik <kovalchykrv@yandex.ru>
2024-01-23 08:59:59 +0300

    Add scanner

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+    System.out.println("For converting from miles into kilometrs enter 1\n" +
+            "For converting from kilometrs into miles enter 2");
+
+Scanner sc = new Scanner(System.in);
+int opcode = sc.nextInt();
+   if(opcode == 1){
+       System.out.println("Enter the value of miles: ");
+       Double ml = sc.nextDouble();
+       System.out.printf(String.valueOf(ml));
+   }
+   else if (opcode==2){
+       System.out.println("Enter the value of kilometres: ");
+       Double km = sc.nextDouble();
+       System.out.printf(String.valueOf(km));
+
+   }
+
+
+
+
+
+    }
+}
